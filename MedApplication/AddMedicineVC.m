188f9e0ba967bf5b052db8c8893a0e1cdb8edc50
sipre <ipenza_samir@live.com>
2013-05-02 22:09:26 -0500

    sdf

--- MedApplication/AddMedicineVC.m
+++ MedApplication/AddMedicineVC.m
@@ -41,6 +41,10 @@ int durationUpperLimit = 31;
     NSLog(@"%@",conteo);
     //Birrueta 2150
     nameTextField.text = @"Wow, si jala";
+    
+    //notification
+    //[self CreateLocalNotification:[NSDate dateWithTimeIntervalSinceNow:10]];
+
 }
 
 #pragma mark - Name And Type View
@@ -155,6 +159,22 @@ int durationUpperLimit = 31;
     frame.origin.y = 548-216;
     datePicker.frame = frame;
     [UIView commitAnimations];
+}
+
+
+#pragma mark - Notification.
+-(UILocalNotification *) CreateLocalNotification:(NSDate *) myFireDate{
+    
+    UILocalNotification *notification = [UILocalNotification new];
+    [notification setAlertBody:@"It's Time to Take Your Medicnie"];
+    notification.timeZone = [NSTimeZone defaultTimeZone];
+    notification.userInfo = [NSDictionary dictionaryWithObject:@"alarm" forKey:@"alarm"];
+    notification.repeatInterval =NSWeekCalendarUnit;
+    notification.fireDate = myFireDate;
+    notification.soundName = UILocalNotificationDefaultSoundName;
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+    
+    return notification;
 }
 
 
